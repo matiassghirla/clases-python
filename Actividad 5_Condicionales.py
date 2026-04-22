26f4747b8b81c2c8d44bd1524dfc71c4b5c31056
matias_sghirla <matiassghirla@gmail.com>
2026-04-22 13:20:20 -0500

    cambio

---
+++ Actividad 5_Condicionales.py
@@ -0,0 +1,56 @@
+"""a = 3
+if a > 0:
+    print('A is a positive number')# A is a positive number
+else:
+    print('A is a negative number')
+
+
+a = int(input("Inserta un numero: "))
+if a > 0:
+    print("El numero es positivo")
+elif a < 0:
+    print("El numero es negativo")
+if a % 2 == 0:
+    print("El numero es par")
+elif a % 2 != 0:
+    print("El numero es impar")
+else:
+    print("No es par ni impar")
+
+
+a = 0
+if a > 0:
+   if a % 2 == 0:
+       print('A is a positive and even integer')
+   elif:
+       print('A is a positive number')
+elif a == 0:
+   print('A is zero')
+else:
+   print('A is a negative number')
+
+
+numero = int(input("Numero: "))
+if numero != 0:
+    if numero > 0:
+        print("El numero es positivo")
+        if numero % 2 == 0:
+            print("El numero es par")
+        else:
+            print("El numero es impar")
+    else:
+        print("El numero es negativo")
+        if numero % 2 != 0:
+            print("El numero es impar")
+        else :
+            print("El numero es par")
+else:
+    print("El numero es cero")"""
+
+
+print("Ingrese su edad porfavor: ")
+edad = int(input())
+if edad >= 18:
+    print("Eres mayor de edad")
+else:  
+    print("Eres menor de edad")
